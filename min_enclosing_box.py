fix bug in backprop when the input of torch.sqrt() is 0

min_enclosing_box.py
@@ -98,7 +98,8 @@ def point_line_distance_range(lines:torch.Tensor, points:torch.Tensor):
     x = points[..., 0]            # (..., 24, 6)
     y = points[..., 1]            # (..., 24, 6)
     den = (y2-y1)*x - (x2-x1)*y + x2*y1 - y2*x1
-    num = torch.sqrt( (y2-y1).square() + (x2-x1).square() ) + 1e-8
+    # NOTE: the backward pass of torch.sqrt(x) generates NaN if x==0
+    num = torch.sqrt( (y2-y1).square() + (x2-x1).square() + 1e-14 )
     d = den/num         # (..., 24, 6)
     d_max = d.max(dim=-1)[0]       # (..., 24)
     d_min = d.min(dim=-1)[0]       # (..., 24)